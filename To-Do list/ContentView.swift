c45a874e1ee33a8b822858d4f866c68f2faef25e
Alex <alexbaum2021@gmail.com>
2024-10-27 23:33:03 +0100

    Editing tasks Improved navigation and styles

--- To-Do list/ContentView.swift
+++ To-Do list/ContentView.swift
@@ -11,71 +11,68 @@ struct Task: Codable, Identifiable {
     var isCompleted: Bool
 }
 
-
-
 class TaskManager: ObservableObject {
     @Published var tasks: [Task] = [] {
         didSet {
-            saveTasks(tasks) // Сохраняем задачи при любом изменении
+            saveTasks(tasks)
         }
     }
-
+    
     init() {
-        tasks = loadTasks() // Загружаем задачи при инициализации
+        tasks = loadTasks()
     }
-
+    
     func addTask(title: String, description: String, priority: String, dueDate: Date) {
         let newTask = Task(title: title, priority: priority, dueDate: dueDate, description: description, isCompleted: false)
         tasks.append(newTask)
     }
-
+    
     func deleteTask(at offsets: IndexSet) {
-        tasks.remove(atOffsets: offsets) // Удаляем задачу
+        tasks.remove(atOffsets: offsets)
     }
-}
-
-func saveTasks(_ tasks: [Task]) {
-    let filename = getDocumentsDirectory().appendingPathComponent("tasks.json")
-    let encoder = JSONEncoder()
-    if let data = try? encoder.encode(tasks) {
-        try? data.write(to: filename)
-    }
-}
-
-func loadTasks() -> [Task] {
-    let filename = getDocumentsDirectory().appendingPathComponent("tasks.json")
-    if let data = try? Data(contentsOf: filename) {
-        let decoder = JSONDecoder()
-        if let tasks = try? decoder.decode([Task].self, from: data) {
-            return tasks
+    
+    func saveTasks(_ tasks: [Task]) {
+        let filename = getDocumentsDirectory().appendingPathComponent("tasks.json")
+        let encoder = JSONEncoder()
+        if let data = try? encoder.encode(tasks) {
+            try? data.write(to: filename)
         }
     }
-    return []
+    
+    func loadTasks() -> [Task] {
+        let filename = getDocumentsDirectory().appendingPathComponent("tasks.json")
+        if let data = try? Data(contentsOf: filename) {
+            let decoder = JSONDecoder()
+            if let tasks = try? decoder.decode([Task].self, from: data) {
+                return tasks
+            }
+        }
+        return []
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
 }
 
-
-
-func getDocumentsDirectory() -> URL {
-    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-    return paths[0]
+extension DateFormatter {
+    static let taskDateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        return formatter
+    }()
 }
-
 
 struct TaskListView: View {
+    @State private var selectedTask: Task?
     @ObservedObject var taskManager: TaskManager
-//    @State private var isEnabled = false
-   // @Binding var tasks: [Task]
+    @Binding var selectedTab: Int
     var body: some View {
-        let dateFormatter: DateFormatter = {
-            let formatter = DateFormatter()
-            formatter.dateStyle = .medium
-            return formatter
-        }()
-        NavigationStack {
-            
-            
-            List {
-                ForEach($taskManager.tasks) { $task in
+        
+        List {
+            ForEach($taskManager.tasks) { $task in
+                HStack (alignment: .center)  {
                     VStack(alignment: .leading) {
                         Text(task.title)
                             .font(.headline)
@@ -84,169 +81,193 @@ struct TaskListView: View {
                         HStack {
                             Text(task.priority)
                             Spacer()
-                           // Text("Due: \(task.dueDate, style: .date)")
-                            Text("Due: \(dateFormatter.string(from: task.dueDate))")
+                            Text(" \(DateFormatter.taskDateFormatter.string(from: task.dueDate))")
                                 .font(.subheadline)
                                 .foregroundColor(.gray)
                         }
                         .font(.subheadline)
                         .foregroundColor(.gray)
                         
-                        
                         Spacer()
                         
                         HStack {
-                            //                            Text(task.priority)
+                            
+                            
+                            
                             Spacer()
                             Toggle(isOn: $task.isCompleted) {
                                 Text(task.isCompleted ? "Done" : "Not Done")
-                                    .foregroundColor(task.isCompleted ? .green : .red)
+                                    .foregroundColor(task.isCompleted ? .green : .gray)
                             }
                             .toggleStyle(SwitchToggleStyle())
-                            .onChange(of: task.isCompleted) {
-                             //   saveTasks(tasks)
+                            
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                            
                         }
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
                         
-                        
-                        }
-                        .labelsHidden()
                     }
+                    .layoutPriority(1)
+                    Divider()
+                    NavigationLink(destination: EditTaskView(task: $task)) {}
+                    .padding(.trailing)
                     
                 }
-                
-              .onDelete(perform: taskManager.deleteTask)
+                .frame(maxWidth: .infinity)
             }
-            .navigationTitle("Tasks")
-            
-            }
+            .onDelete(perform: taskManager.deleteTask)
+        }
+        .navigationTitle("Tasks")
+        .listStyle(InsetListStyle())
+        
     }
-    
-//    func deleteTask(at offsets: IndexSet) {
-//        tasks.remove(atOffsets: offsets)
-////        //saveTasks(tasks)
-//    }
-    
 }
 
+
+
 struct AddTaskView: View {
-//    @Binding var tasks: [Task]
+    
     @ObservedObject var taskManager: TaskManager
     @Binding var selectedTab: Int
-    @State private var navigateToTasks = false
     @State private var newTask: String = ""
     @State private var newDescription: String = ""
     @State private var selectedPriority = "Medium"
     @State private var dueDate = Date()
     let priorities = ["Low", "Medium", "High"]
-     
+    
     var body: some View {
         
-        NavigationStack {
-            VStack {
-                
-                            
-                TextField("Enter new title", text: $newTask)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-                
-                TextEditor(text: $newDescription)
-                                .frame(height: 200)
-                                .border(Color.gray, width: 1)
-                                .padding()
-                
-               
-                
-                HStack {
-                    Picker(selection: $selectedPriority, label: Text("Priority")) {
-                        ForEach(priorities, id: \.self) { priority in
-                            Text(priority)
-                        }
-                    }
-                    .pickerStyle(SegmentedPickerStyle())
-                    Spacer()
-                    DatePicker("", selection: $dueDate, displayedComponents: [.date])
-                        .datePickerStyle(CompactDatePickerStyle())
-                    
-                }
-                          
-                Button(action: {
-                    
-                    if !newTask.isEmpty {
-                        
-//                        let addTask = Task(title: newTask,
-//                                           description: newDescription,
-//                                           priority: selectedPriority,
-//                                           dueDate: dueDate,
-//                                                isCompleted: false)
-                        //tasks.append(addTask)
-                        taskManager.addTask(title: newTask,
-                                            description: newDescription,
-                                            priority: selectedPriority,
-                                            dueDate: dueDate)
-                        navigateToTasks = true
-                       //saveTasks(tasks)
-                        selectedTab = 0
-                        newTask = ""
-                        newDescription = ""
-                    }
-                }) {
-                    Text("Add Task")
-                        .padding()
-                        .foregroundColor(.white)
-                        .background(Color.blue)
-                        .cornerRadius(10)
-                }
+        
+        VStack {
+            
+            
+            TextField("Enter new title", text: $newTask)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
-                
-               
-                
+            
+            TextEditor(text: $newDescription)
+                .frame(height: 200)
+                .border(Color.gray, width: 1)
+                .padding()
+            
+            HStack {
+                Picker(selection: $selectedPriority, label: Text("Priority")) {
+                    ForEach(priorities, id: \.self) { priority in
+                        Text(priority)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
                 Spacer()
+                DatePicker("", selection: $dueDate, displayedComponents: [.date])
+                    .datePickerStyle(CompactDatePickerStyle())
+                
             }
-            .navigationTitle("Add Task")
+            
+            Button(action: {
+                guard !newTask.isEmpty else { return }
+                
+                taskManager.addTask(title: newTask,
+                                    description: newDescription,
+                                    priority: selectedPriority,
+                                    dueDate: dueDate)
+                selectedTab = 0
+                newTask = ""
+                newDescription = ""
+                selectedPriority = "Medium"
+                dueDate = Date()
+            }) {
+                Text("Add Task")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            .padding()
+            
+            Spacer()
         }
+        .navigationTitle("Add Task")
+        
     }
 }
 
+struct EditTaskView: View {
+    @Binding var task: Task
+    @Environment(\.dismiss) var dismiss
 
-///
-
+    let priorities = ["Low", "Medium", "High"]
+    
+       
+    var body: some View {
+        VStack {
+            TextField("Task Title", text: $task.title)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+                .foregroundColor(task.title.isEmpty ? Color.red : Color.blue)
+                .border(task.title.isEmpty ? Color.red : Color.white, width: 1)
+            
+            TextEditor(text: $task.description)
+                .frame(height: 200)
+                .border(Color.gray, width: 1)
+                .padding()
+            
+            HStack {
+                Picker("Priority", selection: $task.priority) {
+                    ForEach(priorities, id: \.self) { priority in
+                        Text(priority)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                
+                Spacer()
+                
+                DatePicker("Due Date", selection: $task.dueDate, displayedComponents: .date)
+                    .datePickerStyle(CompactDatePickerStyle())
+            }
+            .padding()
+            
+            Button(action: {
+                guard !task.title.isEmpty else { return }
+               
+                dismiss()
+            }) {
+                Text("Save Changes")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            .padding()
+            
+            Spacer()
+        }
+        .navigationTitle("Edit Task")
+    }
+}
 
 struct ContentView: View {
-//    @State private var tasks: [Task] = []
     @StateObject private var taskManager = TaskManager()
     @State private var selectedTab = 0
     var body: some View {
-     
-            
-            
-        TabView(selection: $selectedTab) {
-                    
-                    TaskListView(taskManager: taskManager)
-                        .tabItem {
-                            Label("Tasks", systemImage: "list.bullet")
-                        }
-                        .tag(0)
-
-                  
-                    AddTaskView(taskManager: taskManager, selectedTab: $selectedTab)
-                        .tabItem {
-                            Label("Add Task", systemImage: "plus.circle")
-                        }
-                        .tag(1)
-                }
-            
-
-        .padding()
-//        .onAppear {
-//            tasks = loadTasks()
-//        }
-        
+        NavigationStack {
+            TabView(selection: $selectedTab) {
+                
+                TaskListView(taskManager: taskManager, selectedTab: $selectedTab)
+                    .tabItem {
+                        Label("Tasks", systemImage: "list.bullet")
+                    }
+                    .tag(0)
+                
+                AddTaskView(taskManager: taskManager, selectedTab: $selectedTab)
+                    .tabItem {
+                        Label("Add Task", systemImage: "plus.circle")
+                    }
+                    .tag(1)
+            }
+            .padding()
+        }
     }
     
-    
-
 }
 
 struct ContentView_Previews: PreviewProvider {
